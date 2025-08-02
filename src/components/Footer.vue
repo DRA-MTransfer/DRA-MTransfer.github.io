46b6e93accb50febe29bfb0594399845658a3533
DRA-MTransfer <hujunyao0329@foxmail.com>
2025-08-02 12:00:08 +0800

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+// 页脚需要的链接
+const links = {
+    Nerfies: "https://github.com/nerfies/nerfies.github.io",
+    Vue: "https://cn.vuejs.org/",
+    Vite: "https://cn.vitejs.dev/",
+    ElementPlus: "https://element-plus.org/zh-CN/"
+}
+</script>
+
+<template>
+  
+  <div class="bg">
+    
+    <el-watermark :content="['DRA-MTransfer']" :gap="['60','60']" :z-index="0">
+      <el-row justify="center">
+        <p class="footer">
+          Anonymous AAAI 2026 Submission
+        </p>
+      </el-row>
+    </el-watermark>
+  </div>
+</template>
+
+<style scoped>
+
+/* 页脚背景 */
+.bg {
+  background-color: rgb(235, 235, 235);
+}
+
+/* 页脚文字 */
+.footer {
+  text-align: center;
+  margin: 20px;
+  z-index: 1;
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<script>
+import Title from './sections/Title.vue'
+import Abstract from './sections/Abstract.vue'
+import Pipeline from './sections/Pipeline.vue'
+import Result from './sections/Result.vue'
+
+export default {
+  components: {
+    Title,
+    Abstract,
+    Pipeline,
+    Result,
+  }
+}
+</script>
+
+<template>
+  <Title/>
+  <Abstract/>
+  <Pipeline/>
+  <Result/>
+</template>
